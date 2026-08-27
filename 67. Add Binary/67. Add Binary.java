707b78b0d2995c80f020dd7a5910794ade7bc717
NandhithaSelvaraj <nandhitha0637@gmail.com>
2026-08-27 17:23:28 +0530

    Add solution: 67. Add Binary

---
+++ 67. Add Binary/67. Add Binary.java
@@ -0,0 +1,13 @@
+/*
+ * Problem: 67. Add Binary
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/add-binary/submissions/2121714259/
+ * Language: java
+ * Date: 2026-08-27
+ */
+
+class Solution {
+    public String addBinary(String a, String b) {
+        
+    }
+}
